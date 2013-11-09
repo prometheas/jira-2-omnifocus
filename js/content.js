ce31e20d61da6325e9432354555ec481345438de
John Lianoglou <yanni@Aeolus.local>
2013-11-09 01:21:35 -0500

    The extension can now send tasks to the app or via Mail Drop
    The options page added in a previous commit can be used to configure whether items are submitted via app URL or by Mail Drop email.
    
    Signed-off-by: John Lianoglou <yanni@Aeolus.local>

--- js/content.js
+++ js/content.js
@@ -3,13 +3,33 @@ $(function() {
 	var $sendButton = $("<a id=\"send-to-omnifocus\">Send to OmniFocus &raquo;</a>");
 	$sendButton.prependTo( "#viewissuesidebar" );
 
+	function encodeTaskProperties(omnifocusTask) {
+		return {
+			name: encodeURIComponent(omnifocusTask.name),
+			note: encodeURIComponent(omnifocusTask.note)
+		}
+	}
+
+	function triggerActionUrl( url ) {
+		document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+url+'" style="display:none">');
+	}
+
+	var senders = {
+		app: function(config, encodedTask) {
+			triggerActionUrl('omnifocus:///add?name='+encodedTask.name+'&note='+encodedTask.note);
+		},
+		maildrop: function(config, encodedTask) {
+			triggerActionUrl("mailto:"+config.address+"@sync.omnigroup.com?subject="+encodedTask.name+"&body="+encodedTask.note);
+		}
+	}
+
 	$sendButton.on('click', function( evt ) {
 
 		evt.preventDefault();
 
 		var ticket = {
-			key: $("#key-val").text(),
-			summary: $("#summary-val").text().replace( /\s+/, ' ' ).replace( /^\s*(\S.+\S)\s*$/, '$1' ),
+			key        : $("#key-val").text(),
+			summary    : $("#summary-val").text().replace( /\s+/, ' ' ).replace( /^\s*(\S.+\S)\s*$/, '$1' ),
 			description: $("#description-val").text()
 		};
 
@@ -18,10 +38,10 @@ $(function() {
 			note: window.location.href +"\n\n"+ ticket.description
 		};
 
-		var omnifocusUrl = 'omnifocus:///add?name='+encodeURIComponent(omnifocusTask.name)+'&note='+encodeURIComponent(omnifocusTask.note);
+		chrome.extension.sendMessage({method: "loadConfig"}, function(config) {
+			senders[ config.sender ]( config, encodeTaskProperties(omnifocusTask) );
+		});
 
-		// window.location = url does not work repetitively because of http://code.google.com/p/chromium/issues/detail?id=104853
-		document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+omnifocusUrl+'" style="display:none">');
-	})
+	});
 
 });
