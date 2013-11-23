5aec556f3ff5f6fdbdfb6be4d5706e465c942c2c
John Lianoglou <yanni@Aeolus.local>
2013-11-23 01:10:54 -0500

    Task creation now happens from `events.js`
    Refactored the task creation code to run in the `events.js` file, in the attempt to avoid triggering a security restriction caused by invoking app URL schemes encountered when running JIRA over https.
    
    See issue #4 for more information.
    
    Signed-off-by: John Lianoglou <yanni@Aeolus.local>

--- js/content.js
+++ js/content.js
@@ -1,45 +1,32 @@
 $(function() {
 
+	// creates an OF task using the supplied ticket info
+	function createTaskForTicket(ticket) {
+		var message = {
+			method : "createTask",
+			params : {
+				name: "["+ticket.key+'] '+ticket.summary,
+				note: window.location.href +"\n\n"+ ticket.description
+			}
+		};
+
+		chrome.extension.sendMessage(message, function(data) {
+			//TODO: might want to do something fancy in this callback
+			//console.info("Response %s", data);
+		});
+	}
+
 	var $sendButton = $("<a id=\"send-to-omnifocus\">Send to OmniFocus &raquo;</a>");
 	$sendButton.prependTo( "#viewissuesidebar" );
-
-	function encodeTaskProperties(omnifocusTask) {
-		return {
-			name: encodeURIComponent(omnifocusTask.name),
-			note: encodeURIComponent(omnifocusTask.note)
-		}
-	}
-
-	function triggerActionUrl( url ) {
-		document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+url+'" style="display:none">');
-	}
-
-	var senders = {
-		app: function(config, encodedTask) {
-			triggerActionUrl('omnifocus:///add?name='+encodedTask.name+'&note='+encodedTask.note);
-		},
-		maildrop: function(config, encodedTask) {
-			triggerActionUrl("mailto:"+config.address+"@sync.omnigroup.com?subject="+encodedTask.name+"&body="+encodedTask.note);
-		}
-	}
 
 	$sendButton.on('click', function( evt ) {
 
 		evt.preventDefault();
 
-		var ticket = {
+		createTaskForTicket({
 			key        : $("#key-val").text(),
 			summary    : $("#summary-val").text().replace( /\s+/, ' ' ).replace( /^\s*(\S.+\S)\s*$/, '$1' ),
 			description: $("#description-val").text()
-		};
-
-		var omnifocusTask = {
-			name: "["+ticket.key+'] '+ticket.summary,
-			note: window.location.href +"\n\n"+ ticket.description
-		};
-
-		chrome.extension.sendMessage({method: "loadConfig"}, function(config) {
-			senders[ config.sender ]( config, encodeTaskProperties(omnifocusTask) );
 		});
 
 	});
--- js/events.js
+++ js/events.js
@@ -1,14 +1,39 @@
 
+function encodeTaskProperties(omnifocusTask) {
+	return {
+		name: encodeURIComponent(omnifocusTask.name),
+		note: encodeURIComponent(omnifocusTask.note)
+	}
+}
+
+function triggerActionUrl( url ) {
+	document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+url+'" style="display:none">');
+}
+
+var senders = {
+	app: function(config, encodedTask) {
+		triggerActionUrl('omnifocus:///add?name='+encodedTask.name+'&note='+encodedTask.note);
+	},
+	maildrop: function(config, encodedTask) {
+		triggerActionUrl("mailto:"+config.address+"@sync.omnigroup.com?subject="+encodedTask.name+"&body="+encodedTask.note);
+	}
+}
+
+var actions = {
+	createTask: function(taskInfo) {
+		var sender = localStorage.sender || "app";
+		senders[ sender ](localStorage, encodeTaskProperties(taskInfo));
+		return true;
+	}
+}
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
-	if (request.method == "loadConfig") {
-		sendResponse({
-			address: localStorage.address,
-			sender : localStorage.sender || "app"
-		});
+	var responseData = false;
+
+	if (actions[ request.method ]) {
+		responseData = actions[ request.method ](request.params);
 	}
-	else
-	{
-		sendResponse({});
-	}
+
+	sendResponse( responseData );
 });
