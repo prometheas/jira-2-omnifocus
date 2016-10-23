9a4f372c87800012e0dcdac6c8156222b84a0a23
John Lianoglou <john@prometheas.com>
2016-10-23 02:28:19 -0400

    Rearranging project files... a lot
    Moved the files that are actually used to create the extension itself into the `source` directory, leaving only project files remaining in the root directory. Additionally, the third-party zepto library is now being fetched and built into the extension using bower.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,58 @@
+'use strict';
+
 var gulp = require('gulp');
 var del = require('del');
+var bower = require('gulp-bower');
 var sass = require('gulp-sass');
+var shell = require('gulp-shell');
 
 var eslint = require('gulp-eslint');
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('clean', function clean() {
-  return del(['./build/']);
+  return del(['./build/', './vendor/']);
 });
+
+gulp.task('bower:install', function bowerInstall() {
+  return bower();
+});
+
+gulp.task('bower:build-zepto', ['bower:install'], shell.task('npm install && npm run dist', {
+  cwd: 'vendor/zeptojs'
+}));
+
+gulp.task('build:add-vendor-js', ['bower:build-zepto'], function vendorDeps() {
+  return gulp
+    .src('vendor/**/*.min.js')
+    .pipe(gulp.dest('build/chrome/js/vendor'))
+    .pipe(gulp.dest('build/firefox/js/vendor'))
+    .pipe(gulp.dest('build/safari/js/vendor'));
+});
+
+// gulp.task('vendor:build', )
 
 gulp.task('build:js', function buildJs() {
   return gulp
-    .src('js/*.js')
+    .src('source/js/*.js')
     .pipe(gulp.dest('build/chrome/js'))
     .pipe(gulp.dest('build/firefox/js'))
     .pipe(gulp.dest('build/safari/js'));
+});
+
+gulp.task('build:main', function buildMain() {
+  return gulp
+    .src('source/*.*')
+    .pipe(gulp.dest('build/chrome'))
+    .pipe(gulp.dest('build/firefox'))
+    .pipe(gulp.dest('build/safari'));
+});
+
+gulp.task('build:images', function buildImages() {
+  return gulp
+    .src('source/images/**')
+    .pipe(gulp.dest('build/chrome/images'))
+    .pipe(gulp.dest('build/firefox/images'))
+    .pipe(gulp.dest('build/safari/images'));
 });
 
 gulp.task('build:scss', function buildCss() {
@@ -28,7 +66,7 @@ gulp.task('build:scss', function buildCss() {
 
 gulp.task('lint:js', function lintJs() {
   return gulp
-    .src('js/*.js')
+    .src('source/js/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
@@ -41,7 +79,7 @@ gulp.task('lint:scss', function lintScss() {
     .pipe(scsslint.failReporter());
 });
 
-gulp.task('build', ['build:scss', 'build:js']);
+gulp.task('build', ['build:scss', 'build:js', 'build:main', 'build:images', 'build:add-vendor-js']);
 gulp.task('lint', ['lint:js', 'lint:scss']);
 
 gulp.task('default', ['build']);
